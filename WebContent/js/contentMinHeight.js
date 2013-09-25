3c450072200e79e370a6fdc0dea2c987b6379f50
Yury Yaroshevich <Yury_Yaroshevich@epam.com>
2013-09-25 13:49:09 +0300

    work on checkboxes

--- WebContent/js/contentMinHeight.js
+++ WebContent/js/contentMinHeight.js
@@ -2,12 +2,9 @@ $(document).ready(function() {
 	var screenHeight = $(window).height();
 	var headerHeight = $("#header").height()+2*parseInt($("#header").css('border-top-width'));
 	var footerHeight = $("#footer").height()+2*parseInt($("#footer").css('border-top-width'));
-	
-	var tableBorderWidth;
-	if ($("table td").css('border-bottom-width') == undefined) {
-		// there are no tables on a page
-		tableBorderWidth = 0;
-	} else {
+
+	var tableBorderWidth = 0;
+	if ($("table td").css('border-bottom-width') != undefined) {
 		// there is some table
 		tableBorderWidth = $("table > tr").length * parseInt($("table td").css('border-bottom-width'));
 	}
@@ -19,5 +16,6 @@ $(document).ready(function() {
 		2*parseInt($("#content").css('border-top-width')) -
 		2*parseInt($("body").css('border-top-width')) -
 		footerHeight - tableBorderWidth;
+
 	$("#content").css('min-height',contentMinHeight+'px');
 });
---
+++ src/com/epam/ta/component/HtmlSelectManyCheckboxRenderer.java
@@ -0,0 +1,7 @@
+package com.epam.ta.component;
+
+import javax.faces.render.Renderer;
+
+public class HtmlSelectManyCheckboxRenderer extends Renderer {
+
+}
